a850fbc264bf903bece9d31eba1a09c6bbd33b5d
Scaffolder <scaffolder@backstage.io>
2024-06-11 01:46:22 +0000

    initial commit

---
+++ src/main/java/works/integration/adamjava/adamjavaApplication.java
@@ -0,0 +1,13 @@
+package works.integration.adamjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class adamjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(adamjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/works/integration/adamjava/adamjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package works.integration.adamjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class adamjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
